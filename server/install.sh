0afaef0feb27067d37881f2b602542c9e2dfd12d
cyberpwn <dan@arcane.art>
2026-02-14 13:00:37 -0600

    Server Installer

---
+++ server/install.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+set -eu
+
+# Run from: /server/
+# Source:   ../minecraft/
+# Dest:     current directory (/server/)
+
+SRC_BASE="../minecraft"
+
+DIRS="mods config kubejs scripts"
+
+for d in $DIRS; do
+  # Remove existing destination directory to avoid merge-copy
+  rm -rf "./$d"
+
+  # Copy from client to server (preserve attributes when possible)
+  if [ -d "$SRC_BASE/$d" ]; then
+    cp -a "$SRC_BASE/$d" "./"
+  else
+    echo "WARN: source directory missing: $SRC_BASE/$d" >&2
+  fi
+done
+
+echo "Done."
